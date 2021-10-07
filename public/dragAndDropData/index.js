6132bf6d9b044312b051e17f60f4634e8b9ca829
mirella.alves <mirella.alves@gmail.com>
2021-10-07 11:46:07 -0300

    include data and images files

---
+++ public/dragAndDropData/index.js
@@ -0,0 +1,29 @@
+const shapes = [
+  {
+    id: 1,
+    name: 'Circle',
+    img: './images/circle.png'
+  },
+  {
+    id: 2,
+    name: 'Pentagon',
+    img: './images/pentagon.png'
+  },
+  {
+    id: 3,
+    name: 'Triangle',
+    img: './images/triangle.png'
+  },
+  {
+    id: 4,
+    name: 'Square',
+    img: '/images/square.png'
+  },
+  {
+    id: 5,
+    name: 'Heart',
+    img: './images/heart.png'
+  }
+];
+
+export default shapes;
