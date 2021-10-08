817c7788dc73f555dc4138e72c896833eb4d91b3
mirella.alves <mirella.alves@gmail.com>
2021-10-08 12:16:22 -0300

    include items on second column and DragDropContext onDragEnd

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import columns from './dragAndDropData/columns';
 import './App.css';
 
 const App = () => {
-  const [shapesList, setShapesList] = useState(shapes)
+  const [columnsList, setColumnsList] = useState(columns)
 
   const handleOnDragEnd = (result) => {
     console.log(result);
 
-    // const { destination, source } = result;
+    const { destination, source } = result;
 
-    // if (!destination) return;
+    if (!destination) return;
 
-    // const items = Array.from(shapesList);
-    // const [movedItem] = items.splice(source.index, 1);
-    // items.splice(destination.index, 0, movedItem);
+    const [movedItem] = columnsList.splice(source.index, 1);
+    columnsList.splice(destination.index, 0, movedItem);
 
-    // setShapesList(items);
+    setColumnsList(columnsList);
   }
 
   return (
@@ -26,66 +25,38 @@ const App = () => {
       <header>
         <h1>Shapes</h1>
       </header>
-      <DragDropContext>
-            <Droppable droppableId='shapes'>
-                {(provided) => (
-                    <div className='shapes' {...provided.droppableProps} ref={provided.innerRef}>
-                        {columns.map((column) => (
-                          <div className='column'>
-                            {column.cards.map((shape, index) => (
-                                <Draggable key={shape.id} draggableId={shape.id} index={index}>
-                                    {(provided) => (
-                                        <div className='shape' {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                                            <img src={shape.img} alt={`${shape.name} shape`} width='20px' />
-                                            <span>{shape.name}</span>
-                                        </div>
-                                    )}
-                                </Draggable>
-                            ))}
-                          </div>
-                        ))}
-                        {provided.placeholder}
-                    </div>
-                )}
-            </Droppable>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId='shapes'>
+            {(provided) => (
+              <div className='shapes' {...provided.droppableProps} ref={provided.innerRef}>
+                {columnsList.map((column) => (
+                  <div className='column'>
+                    {column.cards.map((shape, index) => (
+                      <div>
+                        {/* {console.log('shape/index:', shape, index)} */}
+                        <Draggable key={shape.id} draggableId={shape.id} index={index}>
+                          {(provided) => (
+                            <div className='shape' {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                              {console.log('shape/index/key:', shape, index, shape.id)}
+                              <img src={shape.img} alt={`${shape.name}`} width='20px' />
+                              <span>{shape.name}</span>
+                            </div>
+                          )}
+                        </Draggable>
+                      </div>
+                    ))}
+                  </div>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
         </DragDropContext>
       <footer>
         <p>@2021</p>
       </footer>
     </div>
   )
-
-  // return (
-  //   <div className='App'>
-  //     <header>
-  //       <h1>Shapes</h1>
-  //     </header>
-  //     <DragDropContext onDragEnd={handleOnDragEnd}>
-  //       <Droppable droppableId='shapes'>
-  //         {(provided) => (
-  //           <ul className='shapes' {...provided.droppableProps} ref={provided.innerRef}>
-  //             { shapesList.map(({ id, name, img }, index) => {
-  //               return (
-  //                 <Draggable key={id} draggableId={id} index={index}>
-  //                   {(provided) => (
-  //                     <li className='shape' {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-  //                       <img src={img} alt={`${name} shape`} width='20px' />
-  //                       <span>{name}</span>
-  //                     </li>
-  //                   )}
-  //                 </Draggable>
-  //               )
-  //             })}
-  //             {provided.placeholder}
-  //           </ul>
-  //         )}
-  //       </Droppable>
-  //     </DragDropContext>
-  //     <footer>
-  //       <p>@2021</p>
-  //     </footer>
-  //   </div>
-  // )
 }
 
 export default App;
--- src/dragAndDropData/columns.js
+++ src/dragAndDropData/columns.js
@@ -1,5 +1,33 @@
 import shapes from './shapes';
 
+const shapes2 = [
+    {
+      id: 'circle2',
+      name: 'Circle2',
+      img: './images/circle.png'
+    },
+    {
+      id: 'pentagon2',
+      name: 'Pentagon2',
+      img: './images/pentagon.png'
+    },
+    {
+      id: 'triangle2',
+      name: 'Triangle2',
+      img: './images/triangle.png'
+    },
+    {
+      id: 'square2',
+      name: 'Square2',
+      img: '/images/square.png'
+    },
+    {
+      id: 'heart2',
+      name: 'Heart2',
+      img: './images/heart.png'
+    }
+  ];
+
 const columns = [
     {
         id: 1,
@@ -9,7 +37,7 @@ const columns = [
     {
         id: 2,
         title: 'column2',
-        cards: shapes,
+        cards: shapes2,
     }
 ];
 
